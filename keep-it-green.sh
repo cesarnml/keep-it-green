81e7e890a416fa2739401460d0ed280346eb197d
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-28 12:11:31 -0800

    stay with the green

--- keep-it-green.sh
+++ keep-it-green.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-cd /Users/cesar/keep-it-green
-echo 'keep this green' >> README.md
-git add --all
-git commit -m "stay with the green"
-git push
+/usr/bin/cd /Users/cesar/keep-it-green
+/bin/echo 'keep this green' >> README.md
+/usr/local/bin/git add --all
+/usr/local/bin/git commit -m "stay with the green"
+/usr/local/bin/git push
